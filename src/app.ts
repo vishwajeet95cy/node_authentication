68ff16c009cc169cb5dc2ada3457be55e732d293
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-09 20:16:28 +0530

    feat: Cors Middleware

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import globalErrorHandler from './middleware/globalErrorHandler'
 import responseMessage from './constant/responseMessage'
 import httpError from './util/httpError'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app: Application = express()
 
 // Middleware
 app.use(helmet())
+app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
